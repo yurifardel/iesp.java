03e032d9eac3cc7c1fc6dd17aec1959dcd81b6a5
yurifardel <yrnascimento@outlook.com>
2021-06-01 22:35:33 -0300

    created login of user

---
+++ src/controllers/LoginUserController.java
@@ -0,0 +1,31 @@
+package controllers;
+
+public class LoginUserController {
+  private String user;
+  private String password;
+
+  public LoginUserController(String user, String password){
+    this.user = user;
+    this.password = password;
+  }
+
+  public String getUser(){
+    return user;
+  }
+  public void setUser(String user){
+    this.user = user;
+  }
+
+  public String getPassword(){
+    return password;
+  }
+  public void setPassword(String password){
+    this.password = password;
+  }
+
+  @Override
+  public String toString(){
+    return "Usuario " + getUser() + " logado";
+  }
+  
+}
