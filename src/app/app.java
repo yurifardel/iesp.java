932556997e153b2b48547b880e2222becc37fa17
yurifardel <yrnascimento@outlook.com>
2021-06-02 21:06:11 -0300

    att

--- src/app/app.java
+++ src/app/app.java
@@ -7,13 +7,8 @@ class App {
   public static LoginUserController login = new LoginUserController("yurifardel", "123456");
   public static FuncionarioController funcionario = new FuncionarioController("Yuri Regis", "Cabedelo", 24, 3500);
   public static void main(String[] args){
-
-    
     System.out.println(register);
     System.out.println(login);
-    System.out.println(funcionario);
-
-
-    
+    System.out.println(funcionario);    
   }
 }
