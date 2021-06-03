6298cf18d30d298bc60c252c886cb45f3479e633
yurifardel <yrnascimento@outlook.com>
2021-06-02 21:03:24 -0300

    main app

--- src/app/app.java
+++ src/app/app.java
@@ -5,11 +5,14 @@ import controllers.*;
 class App {
   public static RegisterController register = new RegisterController("yuri regis nascimento", 24, "cabedelo");
   public static LoginUserController login = new LoginUserController("yurifardel", "123456");
+  public static FuncionarioController funcionario = new FuncionarioController("Yuri Regis", "Cabedelo", 24, 3500);
   public static void main(String[] args){
 
     
     System.out.println(register);
     System.out.println(login);
+    System.out.println(funcionario);
+
 
     
   }
