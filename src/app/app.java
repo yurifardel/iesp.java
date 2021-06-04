5f7926ab8751e6c902757f244ae572459185c834
yurifardel <yrnascimento@outlook.com>
2021-06-03 23:56:31 -0300

    att

--- src/app/app.java
+++ src/app/app.java
@@ -7,13 +7,14 @@ class App {
   public static LoginUserController login = new LoginUserController("yurifardel", "123456");
   public static FuncionarioController funcionario = new FuncionarioController("Yuri Regis", "Cabedelo", 24, 3500);
   public static CPFVoluntarioController cpf = new CPFVoluntarioController("404.504.302-23", "Yuri", 24, "Cabedelo");
-
+  public static CNPJVoluntarioController cnpj =  new CNPJVoluntarioController("404.504.302-23", "AppTech", 0, "Joao pessoa");
   public static void main(String[] args){
     try {
       System.out.println(register);
       System.out.println(login);
       System.out.println(funcionario);
       System.out.println(cpf);
+      System.out.println(cnpj);
 
 
     } catch (Exception err) {
---
+++ src/controllers/CNPJVoluntarioController.java
@@ -0,0 +1,30 @@
+package controllers;
+
+public class CNPJVoluntarioController extends RegisterController{
+  private String CNPJ;
+
+  public CNPJVoluntarioController(String cnpj, String name, int age, String address){
+    super(name, age, address);
+    this.CNPJ = cnpj;
+  }
+
+  public String getCNPJ(){
+    if(this.CNPJ == ""){
+      return "** Informe seu CPF **";
+    }
+    return CNPJ;
+  }
+
+  public void setCNPJ(String cnpj){
+    this.CNPJ = cnpj;
+  }
+
+  @Override
+  public String toString(){
+    return(
+      "Nome da empresa: " + getName() + "\n" +
+      "Endereço da empresa: " + getAddress() + "\n" +
+      "CNPJ da empresa: " + getCNPJ() + "\n" 
+    );
+  }
+}
