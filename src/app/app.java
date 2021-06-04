143c630497cf698287524c4d9ab24fcefc2ef5ad
yurifardel <yrnascimento@outlook.com>
2021-06-04 00:41:52 -0300

    donation

--- src/app/app.java
+++ src/app/app.java
@@ -8,19 +8,21 @@ class App {
   public static FuncionarioController funcionario = new FuncionarioController("Yuri Regis", "Cabedelo", 24, 3500);
   public static CPFVoluntarioController cpf = new CPFVoluntarioController("404.504.302-23", "Yuri", 24, "Cabedelo");
   public static CNPJVoluntarioController cnpj =  new CNPJVoluntarioController("404.504.302-23", "AppTech", 0, "Joao pessoa");
+  public static DonationController donation = new DonationController("em maos", 2000, "1323232323", "");
   public static void main(String[] args){
     try {
-      System.out.println(register);
-      System.out.println(login);
-      System.out.println(funcionario);
-      System.out.println(cpf);
-      System.out.println(cnpj);
+      // System.out.println(register);
+      // System.out.println(login);
+      // System.out.println(funcionario);
+      // System.out.println(cpf);
+      // System.out.println(cnpj);
+      System.out.println(donation);
+
+      
 
 
     } catch (Exception err) {
       System.out.println(err);
     }
-  
-
   }
 }
